05d27e0d46f53be6d5f09edf54bf18cf4310efb1
kemenydani <kemenydani93@gmail.com>
2018-08-23 16:20:15 +0200

    progress

--- layouts/default.vue
+++ layouts/default.vue
@@ -29,9 +29,11 @@
       <v-btn icon @click.stop="miniVariant = !miniVariant">
         <v-icon v-html="miniVariant ? 'chevron_right' : 'chevron_left'"></v-icon>
       </v-btn>
+      <!--
       <v-btn icon @click.stop="clipped = !clipped">
         <v-icon>web</v-icon>
       </v-btn>
+      -->
       <v-toolbar-title v-text="title"></v-toolbar-title>
       <v-spacer></v-spacer>
       
